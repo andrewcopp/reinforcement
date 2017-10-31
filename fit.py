594330cb445aa7e25d7bbe661ca16eb41e3a30ff
Andrew Copp <andrew.w.copp@gmail.com>
2017-10-31 14:35:30 -0400

    Fix Mistakes with Input

--- fit.py
+++ fit.py
@@ -4,8 +4,8 @@ import sys
 
 m = Model()
 
-inputs = sys.argv[2].split(';')
-features = (e.split(',') for e in inputs)
+inputs = sys.argv[2].split('-')
+features = [e.split(',') for e in inputs]
 
 labels = m.fit(sys.argv[1], features)
-sys.stdout.write(','.join(str(e) for e in labels[0]))
+sys.stdout.write(','.join(str(e[0]) for e in labels))
