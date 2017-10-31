52d0d7516b016c93632f64ed388ab3a094a586c3
Andrew Copp <andrew.w.copp@gmail.com>
2017-10-31 15:42:16 -0400

    Fixed Hyphen Issue

--- fit.py
+++ fit.py
@@ -4,7 +4,7 @@ import sys
 
 m = Model()
 
-features = [e.split(',') for e in sys.argv[2].split('-')]
+features = [e.split(',') for e in sys.argv[2].split('_')]
 
 labels = m.fit(sys.argv[1], features)
 sys.stdout.write(','.join(str(e[0]) for e in labels))
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import sys
 
 m = Model()
 
-features = [e.split(',') for e in sys.argv[3].split('-')]
-labels = [e.split(',') for e in sys.argv[4].split('-')]
+features = [e.split(',') for e in sys.argv[3].split('_')]
+labels = [e.split(',') for e in sys.argv[4].split('_')]
 
 m.train(sys.argv[1], sys.argv[2], features, labels)
